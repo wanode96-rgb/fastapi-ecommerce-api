a33778b3eb3e8faffece1e8cc12dfda9a8430e7a
Vaibhav Wanode <wanode96@gmail.com>
2026-03-18 18:22:24 +0530

    fix: hide hashed_password by using response schema for protected endpoint

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -21,6 +21,6 @@ async def register_user(
 
     return new_user
 
-@router.get("/me")
+@router.get("/me", response_model=UserResponse)
 async def get_me(current_user: User = Depends(get_current_user)):
     return current_user
